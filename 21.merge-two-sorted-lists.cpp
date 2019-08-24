a48639412c31625b1f41360938712281b0da07f3
wangxing <wangxing0608@gmail.com>
2019-08-24 22:48:09 +0800

    problem21

--- 21.merge-two-sorted-lists.cpp
+++ 21.merge-two-sorted-lists.cpp
@@ -14,7 +14,27 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
+        ListNode *prehead = new ListNode(-1);   // 定义合并链表之前的一个头结点
+        ListNode *prev = prehead;
+        // 两个链表都不为空, 则合并
+        while (l1 != nullptr && l2 != nullptr)
+        {
+            if (l1 -> val <= l2 -> val)
+            {
+                prev -> next = l1;
+                l1 = l1 -> next;
+            }
+            else
+            {
+                prev -> next = l2;
+                l2 = l2 -> next;
+            }
+            prev = prev -> next;
+        }
+        // l1和l2链表中有一个已经为空链表
+        prev -> next = l1 != nullptr ? l1 : l2;
+
+        return prehead -> next;
     }
 };
 
