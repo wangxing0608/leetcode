5a5ed27d61d2f544e532c04f1813ef678a21b918
wangxing <wangxing0608@gmail.com>
2019-08-24 22:48:09 +0800

    problem8

---
+++ 8.string-to-integer-atoi.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=8 lang=cpp
+ *
+ * [8] String to Integer (atoi)
+ */
+class Solution {
+public:
+    int myAtoi(string str) {
+        // 如果字符串为空
+        if (str.empty())
+        {
+            return 0;
+        }
+
+        int sign =1, base = 0, i = 0, n = str.size();
+        
+        // 跳过字符串中的空白字符
+        while (i < n && str[i] == ' ')
+        {
+            ++i;
+        }
+
+        if (i < n && (str[i] == '+' || str[i] == '-'))
+        {
+            sign = (str[i++] == '+') ? 1 : -1;
+        }
+
+        while (i < n && str[i] >= '0' && str[i] <= '9')
+        {
+            if (base > INT_MAX / 10 || (base == INT_MAX / 10 - '0' > 7))
+            {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            base = 10 * base + (str[i++] - '0');
+        }
+        return base * sign;
+    }
+};
+
