e6246d03a1056b4d99fefe2a96b926cd17a4d71a
wangxing0608 <wangxing0608@gmail.com>
2019-08-30 21:28:17 +0800

    problem198

--- 198.house-robber.cpp
+++ 198.house-robber.cpp
@@ -6,7 +6,20 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        // 如果整数列表为空或只有一个元素
+        if (nums.size() <= 1)
+        {
+            return nums.empty() ? 0 : nums[0];
+        }
         
+        // 使用dp数组记录到各个位置的最大和
+        // 基本情况dp[0] = nums[0], dp1 = max(nums[0], nums[1])
+        vector<int> dp = {nums[0], max(nums[0], nums[1])};
+        for (int i = 2; i < nums.size(); ++i)
+        {
+            dp.push_back(max(nums[i] + dp[i-2], dp[i-1]));
+        }
+        return dp.back();
     }
 };
 
