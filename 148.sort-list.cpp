a1fea2ccf7ca1edf321ab7eb79fa134930626946
wangxing0608 <wangxing0608@gmail.com>
2019-08-30 20:34:09 +0800

    problem148

--- 148.sort-list.cpp
+++ 148.sort-list.cpp
@@ -40,7 +40,56 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        
+        // 边界条件
+        if (head == nullptr || head -> next == nullptr)
+        {
+            return head;
+        }
+
+        // 使用快慢指针找到链表的中间节点
+        ListNode *fast = head;
+        ListNode *slow = head;
+        ListNode *pre = head;
+        while (fast && fast -> next)
+        {
+            pre = slow;
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+
+        pre -> next = nullptr;
+        return merge(sortList(head), sortList(slow)); 
+    }
+
+    ListNode* merge(ListNode *l1, ListNode *l2)
+    {
+        ListNode *dummy = new ListNode(-1);  // dummy指向合并后的有序链表
+        ListNode *cur = dummy;
+        // 两个链表都不为空
+        while (l1 && l2)
+        {
+            if (l1 -> val <= l2 -> val)
+            {
+                cur -> next = l1;
+                l1 = l1 -> next;
+            }
+            else
+            {
+                cur -> next = l2;
+                l2 = l2 -> next;
+            }
+            cur = cur -> next;
+        }
+
+        if (l1)
+        {
+            cur -> next = l1;
+        }
+        if (l2)
+        {
+            cur -> next = l2;
+        }
+        return dummy -> next;
     }
 };
 
---
+++ 198.house-robber.cpp
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=198 lang=cpp
+ *
+ * [198] House Robber
+ */
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+    }
+};
+
