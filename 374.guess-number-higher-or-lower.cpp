a093bbfc9eb22b87ec99f8a9e5db95ab8aa13a1a
wangxing <wangxing0608@gmail.com>
2019-09-17 21:06:25 +0800

    problem374

--- 374.guess-number-higher-or-lower.cpp
+++ 374.guess-number-higher-or-lower.cpp
@@ -47,7 +47,31 @@ int guess(int num);
 class Solution {
 public:
     int guessNumber(int n) {
-        
+        if (guess(n) == 0)
+        {
+            return n;
+        }
+
+        int left = 1;   // 左边界
+        int right = n;  // 右边界
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            int t = guess(mid);
+            if (t == 0)
+            {
+                return mid;
+            }
+            if (t == 1)
+            {
+                left = mid + 1;
+            }
+            else 
+            {
+                right = mid;
+            }
+        }
+        return left;
     }
 };
 
